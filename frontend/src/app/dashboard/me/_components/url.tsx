ed5b63dca516f28b49dd186413015aff9a39e446
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-26 23:57:43 -0300

    feat: add username creation, public URL display and responsive layout

--- frontend/src/app/dashboard/me/_components/url.tsx
+++ frontend/src/app/dashboard/me/_components/url.tsx
@@ -1,8 +1,19 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import { createUsername } from "../_actions/create-username";
+import { useState } from "react";
+import Link from "next/link";
+import { Link2 } from "lucide-react";
 
-export function UrlPreview() {
+interface UrlPreviewProps {
+    username: string | null
+}
+
+export function UrlPreview({ username: slug }: UrlPreviewProps) {
+
+    const [error, setError] = useState<null | string>(null)
+    const [username, setUsername] = useState(slug)
 
     async function submitAction(formData: FormData) {
 
@@ -11,40 +22,80 @@ export function UrlPreview() {
         if (username === "") {
             return
         }
+        // console.log(username)
 
-        
-        console.log(username)
+        const response = await createUsername({ username })
+        // console.log(response)
+        if (response.error) {
+            setError(response.error)
+            return
+        }
+
+        if (response.data) {
+            setUsername(response.data)
+        }
+    }
+
+    if (!!username) {
+
+        return (
+
+            <div className="flex items-center justify-between flex-1 p-2 text-gray-100 flex-wrap">
+                <div
+                    className="flex flex-col md:flex-row items-start md:items-center justify-center flex-wrap gap-2 whitespace-normal break-all"
+                >
+                    <h3 className="font-bold text-lg">Sua URL:</h3>
+                    <Link
+                        href={`${process.env.NEXT_PUBLIC_HOST_URL}/creator/${username}`}
+                        className="break-all"
+                    >
+                        {process.env.NEXT_PUBLIC_HOST_URL}/creator/{username}
+                    </Link>
+                </div>
+
+                <Link href={`${process.env.NEXT_PUBLIC_HOST_URL}/creator/${username}`}
+                    target="_blank"
+                    className="bg-blue-500 px-4 py-1 rounded-md"
+                >
+                    <Link2 className="w-5 h-5 text-white" />
+                </Link>
+            </div>
+        )
     }
 
     return (
 
-        <div className="flex items-center flex-1 p-2 text-gray-100">
-            <form
-                className="flex flex-1 flex-col md:flex-row gap-4 items-start md:items-center"
-                action={submitAction}
-            >
-                <div
-                    className="flex items-center justify-center w-full "
+        <div className="w-full">
+            <div className="flex items-center flex-1 p-2 text-gray-100">
+                <form
+                    className="flex flex-1 flex-col md:flex-row gap-4 items-start md:items-center"
+                    action={submitAction}
                 >
-                    <p className="">
-                        {process.env.NEXT_PUBLIC_HOST_URL}/creator/
-                    </p>
+                    <div
+                        className="flex items-center justify-center w-full flex-wrap"
+                    >
+                        <p className="">
+                            {process.env.NEXT_PUBLIC_HOST_URL}/creator/
+                        </p>
 
-                    <input
-                        type="text"
-                        className="flex-1 outline-none border h-9 border-gray-300 bg-gray-50 text-black rounded-md px-1"
-                        placeholder="Digite seu username..."
-                        name="username"
-                    />
-                </div>
+                        <input
+                            type="text"
+                            className="flex-1 outline-none border h-9 border-gray-300 bg-gray-50 text-black rounded-md px-1"
+                            placeholder="Digite seu username..."
+                            name="username"
+                        />
+                    </div>
 
-                <Button
-                    type="submit"
-                    className="bg-blue-500 hover:bg-blue-600 h-9 w-full md:w-fit text-white px-4 rounded-md cursor-pointer"
-                >
-                    Salvar
-                </Button>
-            </form>
+                    <Button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-600 h-9 w-full md:w-fit text-white px-4 rounded-md cursor-pointer"
+                    >
+                        Salvar
+                    </Button>
+
+                </form>
+            </div>
+            {error && <p className="text-red-500">{error}</p>}
         </div>
     )
 }
