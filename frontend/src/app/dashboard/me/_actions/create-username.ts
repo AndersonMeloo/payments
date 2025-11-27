babaa3aeef3736b38f6d3cb85ecf4accb1f7c98c
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-26 23:58:32 -0300

    feat: new folder createusername, zod

---
+++ frontend/src/app/dashboard/me/_actions/create-username.ts
@@ -0,0 +1,81 @@
+"use server"
+
+import { z } from "zod";
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { createSlug } from "@/utils/create-slug";
+
+const createUsernameSchema = z.object({
+
+    username: z.string({ message: 'Username obrigatório' }).min(4, 'O username precisa ter no minimo 4 caracteres')
+})
+
+type CreateUsernameFormData = z.infer<typeof createUsernameSchema>
+
+export async function createUsername(data: CreateUsernameFormData) {
+
+    const session = await auth()
+
+    if (!session?.user) {
+        return {
+            data: null,
+            error: 'Usuário não autenticado'
+        }
+    }
+
+    const schema = createUsernameSchema.safeParse(data)
+    if (!schema.success) {
+        console.log(schema)
+        return {
+            data: null,
+            error: schema.error.issues[0].message
+        }
+    }
+
+    console.log(data.username)
+
+    try {
+
+        const userId = session.user.id
+
+        const slug = createSlug(data.username)
+        // console.log(slug)
+
+        const existSlug = await prisma.user.findFirst({
+
+            where: {
+                username: slug
+            }
+        })
+
+        if (existSlug) {
+
+            return {
+                data: null,
+                error: 'Este username já existe, tente outro...'
+            }
+        }
+
+        await prisma.user.update({
+
+            where: {
+                id: userId,
+            },
+
+            data: {
+                username: slug
+            }
+        })
+
+        return {
+            data: slug,
+            error: null
+        }
+
+    } catch (error) {
+        return {
+            data: null,
+            error: 'Falha ao atualizar username'
+        }
+    }
+}
