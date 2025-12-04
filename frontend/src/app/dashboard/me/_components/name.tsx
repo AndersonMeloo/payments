08903345828b100e88458fe5cd8a694b3fd66213
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-12-03 22:41:51 -0300

    feat: create name

---
+++ frontend/src/app/dashboard/me/_components/name.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { ChangeEvent, useState } from "react";
+
+function Name({ initialName }: { initialName: string }) {
+
+    const [name, setName] = useState(initialName)
+    const [originalName] = useState(initialName)
+
+    function handleChangeName(e: ChangeEvent<HTMLInputElement>) {
+
+        const value = e.target.value
+        setName(value)
+    }
+
+    return (
+
+        <div>
+            <input
+                className="text-xl md:text-2xl font-bold bg-gray-50 border border-gray-100 rounded-md outline-none p-2 w-full max-w-2xl text-center my-3"
+                value={name}
+                onChange={handleChangeName}
+            />
+        </div>
+    );
+}
+
+export default Name;
